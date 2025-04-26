9549e947348e28722a66b087c4bfaa214c6d5029
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-04-25 23:29:34 -0400

    Time: 0 ms (100%), Space: 42.7 MB (60.44%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -2,14 +2,13 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int ans =0;
         for(String s : operations){
-            if(s.charAt(0)=='+'||s.charAt(2)=='+'){
+            if(s.charAt(1)=='+'){
                 ans++;
             }
-            if(s.charAt(0)=='-'||s.charAt(2)=='-'){
+            if(s.charAt(1)=='-'){
                 ans--;
             }
         }
-
         return ans;
     }
 }
