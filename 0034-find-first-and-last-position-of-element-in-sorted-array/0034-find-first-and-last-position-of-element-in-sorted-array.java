8d6cf549b57ba7c14d6dfa5d96bbe5bae7e7a899
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-04-27 02:26:30 -0400

    Time: 0 ms (100%), Space: 46 MB (47.75%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,68 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l=0, r =nums.length-1;
-        int m=0;
-        if(nums.length==0){
-            return new int[] {-1,-1};
-        }
-        while(l<=r){
-        m = l + (r - l) / 2;
-        if(nums[m]<target){
-            l = m+1;
-            //m = (r+l)/2;
-        }else if(nums[m]>target){
-            r= m-1;
-            //m = (r-l)/2;
-        }else{
-            break;
-        }
-        
-        }
-        // l=m;
-        // r= m;
-        System.out.println(m);
-        //System.out.println(l);
-        if(nums[m]!=target){
-            return new int[] {-1,-1};
-        }else{
-            while((l<=m && nums[l]<target) || (r>=m && nums[r]>target)){
-                if(l<=m && nums[l]<target){
-                    l++;
-                }
-                if(r>=m && nums[r]>target){
-                    r--;
-                }
-            }
-        }
-        //     while((l>0 && nums[l-1]==target) || (r< nums.length-1 && nums[r+1]==target)){
-        //         if(l>0 && nums[l-1]==target ){
-        //             l--;
-        //         }
-        //         if(r<nums.length-1 && nums[r+1]==target){
-        //             r++;
-        //         }
-        //     } 
-        // }else{
-            
+        // int l=0, r =nums.length-1;
+        // int m=0;
+        // if(nums.length==0){
+        //     return new int[] {-1,-1};
         // }
-        return new int[] {l,r};
+        // while(l<=r){
+        // m = l + (r - l) / 2;
+        // if(nums[m]<target){
+        //     l = m+1;
+        //     //m = (r+l)/2;
+        // }else if(nums[m]>target){
+        //     r= m-1;
+        //     //m = (r-l)/2;
+        // }else{
+        //     break;
+        // }
+        
+        // }
+        // // l=m;
+        // // r= m;
+        // System.out.println(m);
+        // //System.out.println(l);
+        // if(nums[m]!=target){
+        //     return new int[] {-1,-1};
+        // }else{
+        //     while((l<=m && nums[l]<target) || (r>=m && nums[r]>target)){
+        //         if(l<=m && nums[l]<target){
+        //             l++;
+        //         }
+        //         if(r>=m && nums[r]>target){
+        //             r--;
+        //         }
+        //     }
+        // }
+        // return new int[] {l,r};
+        //MUCH BETTER SOLUTION
+         int first = findPosition(nums, target, true);
+        int last = findPosition(nums, target, false);
+        
+        return new int[] { first, last };
+
+    }
+        private int findPosition(int[] nums, int target, boolean findFirst) {
+        int left = 0;
+        int right = nums.length - 1;
+        int pos = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                pos = mid;
+                if (findFirst) 
+                    right = mid - 1;
+                else 
+                    left = mid + 1;
+            } 
+            else if (nums[mid] < target) 
+                left = mid + 1;
+            else 
+                right = mid - 1;
+        }
+
+        return pos;
     }
 }
