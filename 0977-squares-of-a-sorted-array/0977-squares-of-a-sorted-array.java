9a124b02a0b073b6d5ded4357eabc48ad93f85be
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-04-28 00:33:11 -0400

    Time: 1 ms (100%), Space: 47.3 MB (53.76%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -10,26 +10,48 @@ class Solution {
         //         q.addLast(t);
         //     }
         // }
-        if(nums.length==1){
+
+    // Below works with 45% Time Complexity and 98% Space Complexity    
+    //     if(nums.length==1){
+    //         return new int[] {nums[0]*nums[0]};
+    //     }
+    //     int i=0;
+    //     Stack<Integer> s = new Stack<>();
+    //     while(i< nums.length && nums[i]<0){
+    //         s.push(Math.abs(nums[i]));
+    //         i++;
+    //     }
+    //     int[] ans = new int[nums.length];
+        
+    //     for(int j=0;j<ans.length;j++){
+    //         if(s.isEmpty() || (i<nums.length && nums[i]<s.peek())){
+    //             ans[j] = nums[i]*nums[i];
+    //             i++;
+    //         }else{
+    //             int temp = s.pop();
+    //             ans[j]= temp* temp;
+    //         }
+    //     }
+    // return ans;
+
+
+    //Two Pointer Approach Now..
+
+    if(nums.length==1){
             return new int[] {nums[0]*nums[0]};
         }
-        int i=0;
-        Stack<Integer> s = new Stack<>();
-        while(i< nums.length && nums[i]<0){
-            s.push(Math.abs(nums[i]));
-            i++;
+    int l=0, r = nums.length-1;
+    int[] ans = new int[nums.length];
+    for(int i=nums.length-1;i>=0;i--){
+        if(Math.abs(nums[l])<Math.abs(nums[r])){
+            ans[i] = nums[r]*nums[r];
+            r--;
+
+        }else{
+            ans[i] = nums[l]*nums[l];
+            l++;
         }
-        int[] ans = new int[nums.length];
-        
-        for(int j=0;j<ans.length;j++){
-            if(s.isEmpty() || (i<nums.length && nums[i]<s.peek())){
-                ans[j] = nums[i]*nums[i];
-                i++;
-            }else{
-                int temp = s.pop();
-                ans[j]= temp* temp;
-            }
-        }
+    }
     return ans;
     }
 }
