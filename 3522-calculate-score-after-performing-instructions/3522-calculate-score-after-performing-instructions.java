626db260feaf40a8e570cf8ab071c41ee48461ce
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-05-07 16:54:29 -0400

    Time: 1 ms (100%), Space: 70.2 MB (37.94%) - LeetHub

--- 3522-calculate-score-after-performing-instructions/3522-calculate-score-after-performing-instructions.java
+++ 3522-calculate-score-after-performing-instructions/3522-calculate-score-after-performing-instructions.java
@@ -2,16 +2,13 @@ class Solution {
     public long calculateScore(String[] instructions, int[] values) {
         long score = 0;
         int i=0;
-        while(i>=0 && i<instructions.length){
-            if("jump".equals(instructions[i])){
-                instructions[i] = "X";
-                i+=values[i];
-            }else if("add".equals(instructions[i])){
-                instructions[i] = "X";
-                score += (int)values[i];
-                i++;
+        boolean[] set = new boolean[instructions.length];
+        while(i>=0 && i<instructions.length && !set[i]){
+            set[i] = true;
+            if(instructions[i].charAt(0)=='a'){
+                score += values[i++];
             }else{
-                break;
+                i+=values[i];
             }
         }
         return score;
