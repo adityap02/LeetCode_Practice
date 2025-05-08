b2265461f821553cb09483fb6ec8ed72a2ba7c96
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-05-08 15:26:22 -0400

    Time: 6 ms (98%), Space: 48.1 MB (36.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,30 +1,44 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> m = new HashMap<>();
-
+        List<List<String>> ans = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>(); 
         for(String s : strs){
-            int[] arr = new int[26];
-            for(char  c : s.toCharArray()){
-                arr[(int)c-(int)'a']++;
-            }
-            String sss = Arrays.toString(arr);
-            addtohashmap(m,sss,s);
+            char[] ar = s.toCharArray();
+            Arrays.sort(ar);
+            String temp = String.valueOf(ar);
+            List<String> st = map.getOrDefault(temp,new ArrayList<>());
+            st.add(s);
+            map.put(temp,st);
         }
-        List<List<String>> ans = new ArrayList<>(m.values());
+        for(String key : map.keySet()){
+            ans.add(map.get(key));
+        } 
         return ans;
-    }
-    private void addtohashmap(Map<String,List<String>> map,String key ,String val){
-        if(map!=null && map.containsKey(key)){
-            List<String> temp = map.get(key);
-            temp.add(val);
-            map.put(key,temp);
-        }
-        else{
-            List<String> temp = new ArrayList<>();
-            temp.add(val);
-            assert map != null;
-            map.put(key,temp);
-        }
+        // Map<String,List<String>> m = new HashMap<>();
 
+        // for(String s : strs){
+        //     int[] arr = new int[26];
+        //     for(char  c : s.toCharArray()){
+        //         arr[(int)c-(int)'a']++;
+        //     }
+        //     String sss = Arrays.toString(arr);
+        //     addtohashmap(m,sss,s);
+        // }
+        // List<List<String>> ans = new ArrayList<>(m.values());
+        // return ans;
     }
+    // private void addtohashmap(Map<String,List<String>> map,String key ,String val){
+    //     if(map!=null && map.containsKey(key)){
+    //         List<String> temp = map.get(key);
+    //         temp.add(val);
+    //         map.put(key,temp);
+    //     }
+    //     else{
+    //         List<String> temp = new ArrayList<>();
+    //         temp.add(val);
+    //         assert map != null;
+    //         map.put(key,temp);
+    //     }
+
+    // }
 }
