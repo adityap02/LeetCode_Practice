f4e3ba74ee02189d5a496badeba9695678062f10
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-05-17 19:51:26 -0400

    Time: 4 ms (97.28%), Space: 48.4 MB (5.53%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,15 +1,44 @@
-class MinStack {
-    Stack<int[]> stack;
+// class MinStack {
+//     Stack<int[]> stack;
     
+//     public MinStack() {
+//         stack = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         if(stack.isEmpty()){
+//             stack.push(new int[] {val,val});
+//         }else{
+//             stack.push(new int[] {val, Math.min(val,stack.peek()[1])});
+//         }
+        
+//     }
+    
+//     public void pop() {
+//         stack.pop();
+//     }
+    
+//     public int top() {
+//         return stack.peek()[0];
+//     }
+    
+//     public int getMin() {
+//         return stack.peek()[1];
+        
+//     }
+// }
+ class MinStack {
+    Stack<Pair<Integer,Integer>> stack;
+
     public MinStack() {
         stack = new Stack<>();
     }
     
     public void push(int val) {
-        if(stack.isEmpty()){
-            stack.push(new int[] {val,val});
+        if(!stack.isEmpty()){
+            stack.add(new Pair(val, Math.min(stack.peek().getValue(), val)));
         }else{
-            stack.push(new int[] {val, Math.min(val,stack.peek()[1])});
+            stack.add(new Pair(val,val));
         }
         
     }
@@ -19,15 +48,22 @@ class MinStack {
     }
     
     public int top() {
-        return stack.peek()[0];
+        return stack.peek().getKey();
     }
     
     public int getMin() {
-        return stack.peek()[1];
-        
+        return stack.peek().getValue();
     }
 }
 
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
