66d0688175fd7533a5cb9fcaf28269f2baad0df4
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-05-31 10:27:08 -0400

    Time: 24 ms (74.67%), Space: 46.6 MB (55.05%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,15 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int sum=0, ans = 0;
-        map.put(0,1);
-        for(int i=0; i<nums.length; i++){
-           sum+= nums[i];
-           if(map.containsKey(sum-k)){
-            ans+=map.get(sum-k);
-           }
-           map.put(sum, map.getOrDefault(sum, 0)+1);
+        Map<Integer,Integer> map = new HashMap<>(); //key -> Sum , val -> num of ways to achieve that Sum
+        map.put(0,1);//base case where no element is selected ( sum =0) , there is 1 way to achieve that 
+        int ans =0, sum =0;
+        for(int i =0; i < nums.length;i++){
+            sum+= nums[i];
+            if(map.containsKey(sum-k)){
+                ans+=map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
         }
         return ans;
+        //Time complexity : O(n). The entire nums array is traversed only once.
+        //Space complexity : O(n). Hashmap map can contain up to n distinct entries in the worst case.
     }
 }
