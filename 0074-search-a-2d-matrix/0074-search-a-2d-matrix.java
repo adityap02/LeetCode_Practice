9f19831788af96e845fbacf52619a7d1e7801ad8
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-06-01 12:57:50 -0400

    Time: 0 ms (100%), Space: 42.2 MB (66.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,10 +2,12 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int r = matrix.length-1, c = matrix[0].length-1;
         int i = 0, j = 0;
-
-        while (i <= r) {
+        //Bin search - O(log mn)
+        while (i <= r) {//this step - calculating row
             int m = (r + i) / 2;
             if (matrix[m][0] < target && (matrix[m][c]-1) >target) {
+                //means target is between 0th index of a row and last index of same row
+                //If this condition is met, it means the target could potentially exist in this row. 
                 break;
             }
             if (target < matrix[m][0]) {
@@ -13,8 +15,8 @@ class Solution {
             } else {
                 i = m + 1;
             }
-
         }
+
         int row = (i+r)/2 ;
         while (j <= c) {
             int m = (j + c) / 2;
