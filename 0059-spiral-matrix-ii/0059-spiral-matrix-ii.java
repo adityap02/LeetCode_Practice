ba9c8fb8a8fa32949903e9d64ba9439bb17f9d1d
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-06-06 23:10:49 -0400

    Time: 0 ms (100%), Space: 41.3 MB (91.99%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,25 +1,40 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] result = new int[n][n];
-        int cnt = 1;
-        for (int layer = 0; layer < (n + 1) / 2; layer++) {
-            // direction 1 - traverse from left to right
-            for (int ptr = layer; ptr < n - layer; ptr++) {
-                result[layer][ptr] = cnt++;
+        int[][] ans = new int[n][n];
+    
+        int num = n*n;
+        int l=0,r=n-1;
+        int counter =1;
+        int rcol = n-1,lcol=0; 
+
+
+        for(int pass =0; pass<(n+1)/2; pass++){
+        //Traversing Layer by layer -> pass = Layer
+        //(n+1)/2 -> gives number of layers 
+            for (int i = pass; i < n - pass; i++) {
+                // direction 1 - traverse from left to right
+                ans[pass][i] = counter++;
             }
-            // direction 2 - traverse from top to bottom
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[ptr][n - layer - 1] = cnt++;
+
+            
+            for (int i = pass + 1; i < n - pass; i++) {
+                // direction 2 - traverse from top to bottom
+                ans[i][n - pass - 1] = counter++;
             }
-            // direction 3 - traverse from right to left
-            for (int ptr = layer + 1; ptr < n - layer; ptr++) {
-                result[n - layer - 1][n - ptr - 1] = cnt++;
+
+            
+            for (int i = pass + 1; i < n - pass; i++) {
+                // direction 3 - traverse from right to left
+                ans[n - pass - 1][n - i - 1] = counter++;
             }
+
             // direction 4 - traverse from bottom to top
-            for (int ptr = layer + 1; ptr < n - layer - 1; ptr++) {
-                result[n - ptr - 1][layer] = cnt++;
+            for (int i = pass + 1; i < n - pass - 1; i++) {
+                ans[n - i - 1][pass] = counter++;
             }
         }
-        return result;
+        
+
+        return ans;
     }
 }
