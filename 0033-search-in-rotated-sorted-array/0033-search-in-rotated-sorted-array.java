b30dbc2f4a081d755402a1c3680cab1413ae06fc
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-07-17 18:37:45 -0400

    Time: 0 ms (100%), Space: 42.3 MB (39.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-       //basic Bin search
-       //if mid large than LeftMost -> means  Left half array is sorted , else Right Half is sorted
-       
-        int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (nums[m] == target) {
+        
+        int l = 0, r = nums.length-1;
+        while(l<=r){
+            int m = (l+r)/2;
+
+            if(nums[m]==target){
                 return m;
-            }
-            //subarray on mid's left is sorted
-            if (nums[l] <= nums[m]) {
-                if (nums[l] <= target && target < nums[m]) {
-                    r = m - 1;
-                } else {
-                    l = m + 1;
+            }else if(nums[l]<=nums[m]){//changeover point is on right
+                if(nums[l]<=target && target<nums[m]){//target is btwn (l & m)
+                    r = m-1;
+                }else{
+                    l = m+1;
                 }
-            } else {//subarray on mid's right is sorted
-                if (nums[m] < target && target <= nums[r]) {
-                    l = m + 1;
-                } else {
-                    r = m - 1;
+            }else{
+                //Changeover poitn is on left
+                if(nums[m]<target && target <=nums[r]){//target btwn( m&r)
+                    l = m+1;
+                }else{
+                    r = m-1;
                 }
             }
+
+
+
+
         }
         return -1;
-
     }
 }
