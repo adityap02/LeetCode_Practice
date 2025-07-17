8aecd99714ee0dc74e7da5dd8b1bb857d99af7a1
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-07-17 18:21:06 -0400

    Time: 0 ms (100%), Space: 42.3 MB (31.55%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,8 +1,6 @@
 class Solution {
     public int findMin(int[] nums) {
-
-        //Goal is to find the changeover point/start of the array, where the arr is rotated
-        int l = 0, r = nums.length - 1, min = 5001;
+        int l = 0, r = nums.length - 1;
         if (nums.length == 1) {
             return nums[0];
         }
@@ -10,30 +8,23 @@ class Solution {
             return nums[0];
             // if the last element is greater than the first element then there is no rotation.
         }
-        while (r >= l) {
-            int m = (l + r) / 2;
-            //min = Math.min(min, nums[m]);
+        int min = Integer.MAX_VALUE;
+        while (l <= r) {
+            int mid = (l + r) / 2;
 
-            if (nums[m] > nums[m + 1]) {
-                return nums[m + 1];
-            }//this means that the m+1 is the changeover point
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            } //this means that the m+1 is the changeover point
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            } //this means that the m is the changeover point
 
-            if (nums[m- 1] > nums[m]) {
-                return nums[m];
-            }//this means that the m is the changeover point
-
-
-            //Otherwise go for binary search as usual
-            if (nums[m] > nums[0]) {
-                l = m + 1;
-            } else {
-                // if nums[0] is greater than the mid value then this means the smallest value
-                // is somewhere to
-                // the left
-                r = m - 1;
+            if (nums[mid] > nums[l]) {
+                l=mid+1;
+            }else{
+                r = mid-1;
             }
         }
         return min;
-
     }
 }
