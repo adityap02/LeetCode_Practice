9fc7b2e64e170074b46da1efc91636d7fba57118
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-07-17 13:08:07 -0400

    Time: 0 ms (100%), Space: 42.3 MB (51.03%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,35 +1,40 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int r = matrix.length-1, c = matrix[0].length-1;
-        int i = 0, j = 0;
-        //Bin search - O(log mn)
-        while (i <= r) {//this step - calculating row
-            int m = (r + i) / 2;
-            if (matrix[m][0] < target && (matrix[m][c]-1) >target) {
-                //means target is between 0th index of a row and last index of same row
-                //If this condition is met, it means the target could potentially exist in this row. 
-                break;
-            }
-            if (target < matrix[m][0]) {
-                r = m - 1;
-            } else {
-                i = m + 1;
-            }
-        }
+        int cols  =matrix.length-1; // no of cols index
+        int n  = matrix[0].length-1;//no of rows index
 
-        int row = (i+r)/2 ;
-        while (j <= c) {
-            int m = (j + c) / 2;
-            if (matrix[row][m] == target) {
+        int l = 0, r =cols;
+        //calculating row
+        while(l<=r){
+            int m = (l+r)/2;
+            if(target == matrix[m][0]){
                 return true;
             }
-            if (target < matrix[row][m]) {
-                c = m - 1;
-            } else {
-                j = m + 1;
+            if(target>matrix[m][0] && target< matrix[m][n] ){
+                break;
             }
-
+            if(target < matrix[m][0]){
+                r= m-1;
+            }else{
+                l=m+1;
+            }
         }
+
+        int row = (l+r)/2 ;
+        l=0;
+        r=n;
+        //finding in that row
+        while(l<=r){
+            int m  = (l+r)/2;
+            if(target==matrix[row][m]){
+                return true;
+            }else if(target<matrix[row][m]){
+                r=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+
         return false;
     }
 }
