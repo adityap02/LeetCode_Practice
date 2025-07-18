d829e8fa1d03766f9fa0f0c81a6851fd0989f39d
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-07-17 20:13:14 -0400

    Time: 150 ms (45.52%), Space: 120.5 MB (44.83%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,34 +1,87 @@
+// class TimeMap {
+//     Map<String, TreeMap<Integer, String>> map;
+//     public TimeMap() {
+//         map = new HashMap<>();
+//     }
+    
+//     public void set(String key, String value, int timestamp) {
+//         if(!map.containsKey(key)){
+//         TreeMap<Integer,String> tMap = new TreeMap<>(Comparator.reverseOrder());
+//         tMap.add(timeStamp,value);
+//         map.put(key,tMap);
+//         }else{
+//             TreeMap<Integer,String> tMap = map.get(key);
+//             tMap.add(timeStamp,value);
+//             //map.put(key,tMap);
+//         }
+//     }
+    
+//     public String get(String key, int timestamp) {
+//         if(map.containsKey(key)){
+//             TreeMap<Integer,String> tMap = map.get(key);
+//             if(tMap.containsKey(timestamp)){
+//                 return tMap.get(timestamp);
+//             }else{
+//                 Integer k = tree.floorKey(timestamp);
+//                 if(k == null){
+//                     return "";
+//                 }else{
+//                     return tree.get(k);
+//                 }
+//             }
+//         }else{
+//             return "";
+//         } 
+//     }
+// }
+
+
+
+//Binary Search
 class TimeMap {
-    Map<String,TreeMap<Integer,String>> map;
+    Map<String, List<Pair<Integer,String>>> map;
     public TimeMap() {
         map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
         if(map.containsKey(key)){
-            TreeMap<Integer,String> tree = map.get(key);
-            tree.put(timestamp,value);
+            List<Pair<Integer,String>> list = map.get(key);
+            list.add(new Pair(timestamp,value));
+            map.put(key,list);
         }else{
-            TreeMap<Integer,String> tree = new TreeMap<>();
-            tree.put(timestamp,value);
-            map.put(key,tree);
+           List<Pair<Integer,String>> list = new ArrayList<>();
+           list.add(new Pair(timestamp,value));
+           map.put(key,list);
         }
     }
     
     public String get(String key, int timestamp) {
-        if(!map.containsKey(key)){
-            return "";
-        }else{
-            TreeMap<Integer,String> tree = map.get(key);
-            if(tree.containsKey(timestamp)){
-                return tree.get(timestamp);
-            }else{
-                      var t =  tree.floorEntry(timestamp);
-        return t != null ? t.getValue() : "";
+        String result = "";
+        if(map.containsKey(key)){
+            List<Pair<Integer,String>> list = map.get(key);
+            int l =0 , r = list.size()-1;
+            //Binary Search
+            while(l<=r){
+                int m = (l+r)/2;
+
+                if(list.get(m).getKey() == timestamp){
+                    return list.get(m).getValue();
+                }else if(list.get(m).getKey()<timestamp){
+                    result= list.get(m).getValue();
+                    l=m+1;
+                }else{
+                    r=m-1;
+                }
             }
+        }else{
+            return "";
         }
+        return result; 
     }
 }
+
+
 
 /**
  * Your TimeMap object will be instantiated and called as such:
