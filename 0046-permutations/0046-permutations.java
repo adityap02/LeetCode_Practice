549fb144582d23735b9803d9a0adb334774cc84a
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-07-21 16:55:09 -0400

    Time: 1 ms (92.71%), Space: 44.6 MB (44.37%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,28 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        //O(n! * n) & SC : O(n! *n)
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(new ArrayList<>(), ans, nums);
+        //Will Maintain arraylist curr to make the permutations, and Boolean Array 
+        //to one by one select each element, pick it backtrack it while keeping others false in it
+        //Basically we are creating permutations by trying to keep all elements at each positions one by one
+        backtrack(nums, new ArrayList<>(), ans,new boolean[nums.length]);
         return ans;
     }
 
-    public void backtrack(
-        List<Integer> curr,
-        List<List<Integer>> ans,
-        int[] nums
-    ) {
-        if (curr.size() == nums.length) {
-            ans.add(new ArrayList<>(curr));
+    void backtrack(int[] nums, List<Integer> cur, List<List<Integer>> ans, boolean[] pick) {
+        if (cur.size() == nums.length) {
+            ans.add(new ArrayList(cur));
             return;
         }
-
-        for (int num : nums) {
-            if (!curr.contains(num)) {
-                curr.add(num);
-                backtrack(curr, ans, nums);
-                curr.remove(curr.size() - 1);
+        for (int i = 0; i < nums.length; i++) {
+            //this will run for each element in array, keeping each element 1st place in permutation
+            //and similarly backtrack will  provide place for all other elements on other positions 
+            if (!pick[i]) {
+                cur.add(nums[i]);
+                pick[i] = true;
+                backtrack( nums, cur, ans, pick);
+                cur.remove(cur.size() - 1);
+                pick[i] = false;
             }
         }
     }
