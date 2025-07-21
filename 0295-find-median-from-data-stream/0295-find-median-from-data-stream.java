969c8ffbfd332b38bc83fc30d0677285c2f29bdb
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-07-21 11:18:40 -0400

    Time: 111 ms (50.62%), Space: 63.7 MB (68.36%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,28 +1,34 @@
 class MedianFinder {
-private PriorityQueue<Integer> small;
-private PriorityQueue<Integer> large;
-private boolean even = true;
+    //Use 2 PQ , min and max
+    private PriorityQueue<Integer> small;
+    private PriorityQueue<Integer> large;
+    //small will store all large nums in ascending order
+    //large will all small elements in descending order
+    // so for median, PQ will have largest num from smaller nums(Large PQ) in case of Odd
+    // and for Even Nums : small.peek() will have smallest num from larger half  and large.peek() will have.. 
+    private boolean even = true;
+
     public MedianFinder() {
         large = new PriorityQueue<>(Collections.reverseOrder());
         small = new PriorityQueue<>();
     }
-    
+
     public void addNum(int num) {
-         if (even) {
-        small.offer(num);
-        large.offer(small.poll());
-    } else {
-        large.offer(num);
-        small.offer(large.poll());
+        if (even) {
+            small.offer(num);
+            large.offer(small.poll());
+        } else {
+            large.offer(num);
+            small.offer(large.poll());
+        }
+        even = !even;
     }
-    even = !even;
-    }
-    
+
     public double findMedian() {
         if (even)
-        return (large.peek() + small.peek()) / 2.0;
-    else
-        return large.peek();
+            return (large.peek() + small.peek()) / 2.0;
+        else
+            return large.peek();
     }
 }
 
