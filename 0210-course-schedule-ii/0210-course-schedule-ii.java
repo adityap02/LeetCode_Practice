ded0c75faf106519bef410a266998d6d82c10b9b
Aditya Porwal <50493250+adityap02@users.noreply.github.com>
2025-07-22 20:25:30 -0400

    Time: 6 ms (44.38%), Space: 46.3 MB (20.29%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,56 +1,52 @@
-class Solution {
+public class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
-        int[] indegree = new int[numCourses];
-        int[] topologicalOrder = new int[numCourses];
-
-        // Create the adjacency list representation of the graph
-        for (int i = 0; i < prerequisites.length; i++) {
-            int dest = prerequisites[i][0];
-            int src = prerequisites[i][1];
-            List<Integer> lst = adjList.getOrDefault(
-                src,
-                new ArrayList<Integer>()
-            );
-            lst.add(dest);
-            adjList.put(src, lst);
-
-            // Record in-degree of each vertex
-            indegree[dest] += 1;
+        //Key -> Course , Val -> All Prerequsites
+        Map<Integer, List<Integer>> prereq = new HashMap<>();
+        for (int[] pair : prerequisites) {
+            prereq.computeIfAbsent(pair[0], 
+                k -> new ArrayList<>()).add(pair[1]);
         }
 
-        // Add all vertices with 0 in-degree to the queue
-        Queue<Integer> q = new LinkedList<Integer>();
+        //A Course is only added to Output if that has no cycle and has no prerequsites remaining
+        //will do this using the cycle and visit Hashsets. Cycle for cycle detection and Visit to eleminate recursion on duplicates
+        //once we reach end of recursion on one node, add it to output as there are no prerequsites left for that 
+        List<Integer> output = new ArrayList<>();
+        Set<Integer> visit = new HashSet<>();
+        Set<Integer> cycle = new HashSet<>();
+        //..If DFS is False , means No Cycle
+        for (int course = 0; course < numCourses; course++) {
+            if (!dfs(course, prereq, visit, cycle, output)) {
+                return new int[0];
+            }
+        }
+
+        int[] result = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
+            result[i] = output.get(i);
+        }
+        return result;
+    }
+
+    private boolean dfs(int course, Map<Integer, List<Integer>> prereq,
+                        Set<Integer> visit, Set<Integer> cycle, 
+                        List<Integer> output) {
+
+        if (cycle.contains(course)) {
+            return false;
+        }
+        if (visit.contains(course)) {
+            return true;
+        }
+
+        cycle.add(course);
+        for (int pre : prereq.getOrDefault(course, Collections.emptyList())) {
+            if (!dfs(pre, prereq, visit, cycle, output)) {
+                return false;
             }
         }
-
-        int i = 0;
-        // Process until the Q becomes empty
-        while (!q.isEmpty()) {
-            int node = q.remove();
-            topologicalOrder[i++] = node;
-
-            // Reduce the in-degree of each neighbor by 1
-            if (adjList.containsKey(node)) {
-                for (Integer neighbor : adjList.get(node)) {
-                    indegree[neighbor]--;
-
-                    // If in-degree of a neighbor becomes 0, add it to the Q
-                    if (indegree[neighbor] == 0) {
-                        q.add(neighbor);
-                    }
-                }
-            }
-        }
-
-        // Check to see if topological sort is possible or not.
-        if (i == numCourses) {
-            return topologicalOrder;
-        }
-
-        return new int[0];
+        cycle.remove(course);
+        visit.add(course);
+        output.add(course);
+        return true;
     }
 }
